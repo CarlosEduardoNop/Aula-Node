0dd88db68bc95483eb19e2b0d98ae793241a8db1
Carlos Eduardo <CarlosEduardoS.C@hotmail.com>
2020-05-20 14:18:47 -0300

    API db.mongo pronta

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
@@ -6,16 +7,10 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true, us
 
 requireDir("./src/models");
 
-const Product = mongoose.model('Product');
-
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req,res) =>{
-    Product.create({
-        title: 'React Native',
-        description: 'Build native app with ...',
-        url: 'http://github.com/facebook/react-native'
-    });
-});
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
